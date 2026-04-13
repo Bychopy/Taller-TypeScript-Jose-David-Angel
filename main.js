c1d3ae68cc52e2fff24e6aecfa801e25b2d328af
Bychopy <jd.angelc12@uniandes.edu.co>
2026-04-13 07:31:52 -0500

    implement promedio de temporadas

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 import { series } from './data.js';
+function calcularPromedioDeTemporadas(series) {
+    if (series.length === 0) {
+        return 0;
+    }
+    const totalSeasons = series.reduce((acc, serie) => acc + serie.seasons, 0);
+    return totalSeasons / series.length;
+}
+function renderPromedioDeTemporadas(series) {
+    const averageElement = document.getElementById('seasons-average');
+    if (!(averageElement instanceof HTMLParagraphElement)) {
+        return;
+    }
+    const average = calculateSeasonsAverage(series);
+    averageElement.textContent = `Seasons average: ${average.toFixed(2)}`;
+}
 function renderSeriesInTable(series) {
     const tableBody = document.getElementById('series-tbody');
     if (!(tableBody instanceof HTMLTableSectionElement)) {
@@ -17,3 +32,4 @@ function renderSeriesInTable(series) {
     }
 }
 renderSeriesInTable(series);
+renderSeasonsAverage(series);
--- main.ts
+++ main.ts
@@ -1,6 +1,26 @@
 import { series } from './data.js';
 import type { Serie } from './serie.js';
 
+function calculateSeasonsAverage(series: Serie[]): number {
+  if (series.length === 0) {
+    return 0;
+  }
+
+  const totalSeasons = series.reduce((acc, serie) => acc + serie.seasons, 0);
+  return totalSeasons / series.length;
+}
+
+function renderSeasonsAverage(series: Serie[]): void {
+  const averageElement = document.getElementById('seasons-average');
+
+  if (!(averageElement instanceof HTMLParagraphElement)) {
+    return;
+  }
+
+  const average = calculateSeasonsAverage(series);
+  averageElement.textContent = `Seasons average: ${average.toFixed(2)}`;
+}
+
 function renderSeriesInTable(series: Serie[]): void {
   const tableBody = document.getElementById('series-tbody');
 
@@ -23,3 +43,4 @@ function renderSeriesInTable(series: Serie[]): void {
 }
 
 renderSeriesInTable(series);
+renderSeasonsAverage(series);
